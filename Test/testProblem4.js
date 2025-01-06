9e52979d38b3d7eba6c20dfd758b3628cf63e203
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:06:14 +0530

    test file for to get all car year.

--- Test/testProblem4.js
+++ Test/testProblem4.js
@@ -1,7 +1,11 @@
+/* ==== Problem #4 ====
+The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the 
+dealer data containing only the car years and log the result in the console as it was returned.
+ */
+
 const inventory = require('../database');
 const getAllCarYear = require('../problem4');
 
-//Problem 4
 let result = getAllCarYear(inventory);
 
 if(result.length != 0) {
