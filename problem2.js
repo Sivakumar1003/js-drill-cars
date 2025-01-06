896d6f953884f72625eb3da5a651586391bb5159
Sivakumar N <shivask94423@gmail.com>
2025-01-06 15:58:34 +0530

    function to get last car.

--- problem2.js
+++ problem2.js
@@ -1,9 +1,3 @@
-/* ==== Problem #2 ====
-The dealer needs the information on the last car in their inventory. Execute a function to find what the make 
-and model of the last car in the inventory is?  Log the make and model into the console in the format of:
-    "Last car is a *car make goes here* *car model goes here*"
-*/
-
 // this is to get last car in inventory.
 function getLastCar(inventory) {
     return inventory[inventory.length-1]; // return last car
