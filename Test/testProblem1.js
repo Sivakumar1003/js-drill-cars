b8bca9d8ede571d01664bb0d0a179e2863f99bf0
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:04:51 +0530

    test file for to get car by id.

--- Test/testProblem1.js
+++ Test/testProblem1.js
@@ -1,7 +1,14 @@
+/* ==== Problem #1 ====
+    The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out 
+    which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, 
+    make, and model in the console log in the format of:
+
+    "Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
+*/
+
 const inventory = require('../database');
 const getCarById = require('../problem1');
 
-// Problem 1
 let result = getCarById(inventory, 33);
 
 if(result) {
