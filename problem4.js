3e4ad76a4d5537ce222da54ba1c8c9b96e2dde93
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:02:54 +0530

    function to get all car years.

--- problem4.js
+++ problem4.js
@@ -1,18 +1,8 @@
-/* ==== Problem #4 ====
-The accounting team needs all the years from every car on the lot. Execute a function that will return an array from the 
-dealer data containing only the car years and log the result in the console as it was returned.
- */
-
 //to get all car year.
 function getAllCarYear(inventory) {
-    let  allCarYear =[];
-   
-    // iterate the inventory to get car year.
-    for(carDetails of inventory) {
-        allCarYear.push(carDetails.car_year);
-    }
-    
-    return allCarYear; // return all car year.
+    return inventory.map( detail => {
+        return detail["car_year"]
+    }); 
 }
 
 module.exports = getAllCarYear;
