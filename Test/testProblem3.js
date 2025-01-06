7ca285939694be98fb97eedaa6d959b6d49173c7
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:05:51 +0530

    test file for to get all car model.

--- Test/testProblem3.js
+++ Test/testProblem3.js
@@ -1,7 +1,11 @@
+/* ==== Problem #3 ====
+The marketing team wants the car models listed alphabetically on the website. Execute a function to Sort all 
+the car model names into alphabetical order and log the results in the console as it was returned.
+*/ 
+
 const inventory = require('../database');
 const getCarModel = require('../problem3');
 
-//problem 3
 let result = getCarModel(inventory);
 
 if(result.length != 0) {
