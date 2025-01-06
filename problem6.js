29847860d44d937588678be6060e50e83d40e20a
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:03:51 +0530

    function to get all Audi and BMW  car only.

--- problem6.js
+++ problem6.js
@@ -1,21 +1,7 @@
-/* ==== Problem #6 ====
-A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only 
-contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-*/
-
 function getBMWAndAudi(inventory) {
-    let BMWAndAudiCars = [];
-
-    //iterate inventory to get all car details
-    for (let i = 0; i < inventory.length; i++) {
-
-        //check only for BMW and Audi car.
-        if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
-            BMWAndAudiCars.push(inventory[i]);
-        }
-    }
-
-    return BMWAndAudiCars; // return all Audi and BMW cars.
+    return inventory.filter(detail => {
+        return detail["car_make"] === "Audi" || detail["car_make"] === "BMW";
+    })
 }
 
 module.exports = getBMWAndAudi;
