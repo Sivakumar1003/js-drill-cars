de032d02668924148440a9d4688eaed018b61ef1
Sivakumar N <shivask94423@gmail.com>
2025-01-06 16:05:25 +0530

    test file for to get last car.

--- Test/testProblem2.js
+++ Test/testProblem2.js
@@ -1,7 +1,12 @@
+/* ==== Problem #2 ====
+The dealer needs the information on the last car in their inventory. Execute a function to find what the make 
+and model of the last car in the inventory is?  Log the make and model into the console in the format of:
+    "Last car is a *car make goes here* *car model goes here*"
+*/
+
 const inventory = require('../database');
 const getLastCar = require('../problem2');
 
-//problem 2
 let result = getLastCar(inventory);
 
 if(result) {
